06561887db7ef241416ae090aa3d72fbea6b802f
Ashutosh <ashutosh27052003@gmail.com>
2025-02-09 00:01:51 +0530

    some changes

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = `https://api.nasa.gov/planetary/apod?api_key=NPB2BvHp0jQaJMCCD9yQavcH8cUPCTcDEzS4lRyY`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        
+        document.querySelector("h2").innerText = data.title
+        document.querySelector("img").src = data.hdurl
+        document.querySelector("h3").innerText = data.explanation
+
       })
       .catch(err => {
           console.log(`error ${err}`)
