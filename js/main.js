dd24c904d0ef3d4b6f10612cf8cb66b24cdf5ce2
Ashutosh <ashutosh27052003@gmail.com>
2025-02-09 00:07:56 +0530

    added query parameters

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = `https://api.nasa.gov/planetary/apod?api_key=NPB2BvHp0jQaJMCCD9yQavcH8cUPCTcDEzS4lRyY`
+  const url = `https://api.nasa.gov/planetary/apod?api_key=NPB2BvHp0jQaJMCCD9yQavcH8cUPCTcDEzS4lRyY&date=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
