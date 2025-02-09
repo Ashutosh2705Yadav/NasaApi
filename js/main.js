9a679cc6177d725a5021bd7a8f883d052dfe2a93
Ashutosh <ashutosh27052003@gmail.com>
2025-02-09 12:18:31 +0530

    added ifram since nasa on some dates posted videos insted of images

--- js/main.js
+++ js/main.js
@@ -11,9 +11,15 @@ function getFetch(){
         console.log(data)
         
         document.querySelector("h2").innerText = data.title
+        if(data.media_type == "image"){
         document.querySelector("img").src = data.hdurl
+        }
+        else if(data.media_type=="video"){
+          document.querySelector("iframe").src = data.url
+        }
+        
         document.querySelector("h3").innerText = data.explanation
-
+       
       })
       .catch(err => {
           console.log(`error ${err}`)
